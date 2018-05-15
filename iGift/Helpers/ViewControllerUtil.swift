9a36c45c3126460775bdd87c0b201887b4224605
Isuru Jayathissa <isuru@CreativeSoftware.com>
2018-05-15 22:50:03 +0530

    add utils

--- iGift/Helpers/ViewControllerUtil.swift
+++ iGift/Helpers/ViewControllerUtil.swift
@@ -10,4 +10,15 @@ import UIKit
 
 class ViewControllerUtil: NSObject {
 
+    class func showAlert(alertTitle:String, alertMessage:String){
+        
+        let perent: UIViewController = (UIApplication.shared.delegate as! AppDelegate).window!.rootViewController!
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        
+        perent.showDetailViewController(alert, sender: nil)
+        
+    }
 }
