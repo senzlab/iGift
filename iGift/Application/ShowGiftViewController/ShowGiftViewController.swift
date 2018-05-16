a02e70cca112d589fedbd806f9327b4081c2695d
AnujAroshA <arosha@creativesoftware.com>
2018-05-16 11:28:40 +0530

    Preview image taken from local & remote

--- iGift/Application/ShowGiftViewController/ShowGiftViewController.swift
+++ iGift/Application/ShowGiftViewController/ShowGiftViewController.swift
@@ -18,7 +18,11 @@ class ShowGiftViewController: BaseViewController {
         print((#file as NSString).lastPathComponent)
         self.setupUi()
         
-        giftImageView.image = UIImage(contentsOfFile: readFileInPath(relativeFilePath: Constants.IMAGES_DIR.rawValue))
+//        Previewing an image by taking from application documents directory
+//        giftImageView.image = UIImage(contentsOfFile: readFileInPath(relativeFilePath: Constants.IMAGES_DIR.rawValue))
+        
+//        Previewing an image from remote URL. Added anujarosha.lk in Info.plist for testing
+        giftImageView.downloadedFrom(link: "http://anujarosha.lk/images/original/my_trip_to_MtLavinia2.jpg")
     }
     
     //    MARK: Supportive functions
