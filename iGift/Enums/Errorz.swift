a9d397275868789341df7630f8a5a7b9482a6710
eranga bandara <eranga.herath@pagero.com>
2018-05-25 23:20:15 +0530

    errorz

---
+++ iGift/Enums/Errorz.swift
@@ -0,0 +1,20 @@
+//
+//  Errorz.swift
+//  iGift
+//
+//  Created by eranga on 5/25/18.
+//  Copyright © 2018 Creative Solutions. All rights reserved.
+//
+
+import Foundation
+
+enum Errorz: Error {
+    case InvalidAccount
+    case InvalidAmount
+    case InvaidPassword
+    case EmptyInput
+    case InvalidInput
+    case MismatchAccount
+    case MismatchPassword
+    case MismatchPhone
+}
